9fec0d1a0e916be88430f7cdceb152617fe25980
Sira <siraphop_2538@hotmail.com>
2023-11-07 16:47:12 +1100

    Add simple email validation

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+class formValidation {
+  email = document.querySelector('.email');
+  allInput = document.querySelectorAll('input');
+  form = document.querySelector('form');
+
+  constructor() {
+    this._emailValidation();
+    this._emptyEmail();
+  }
+
+  //Email
+  _emailValidation() {
+    this.email.addEventListener('input', event => {
+      if (this.email.validity.typeMismatch) {
+        this.email.setCustomValidity(
+          'Please enter your email: turtle@gmail.com'
+        );
+      } else {
+        this.email.setCustomValidity('');
+      }
+
+      this._emptyEmail();
+    });
+  }
+
+  _emptyEmail() {
+    if (this.email.validity.valueMissing) {
+      this.email.setCustomValidity('Please fill in Email');
+    } else {
+      console.log('Email is not empty');
+    }
+  }
+}
+
+const newForm = new formValidation();
