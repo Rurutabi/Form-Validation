85e4dc6e79319cd1cbcc1a691ba33e6450c46ffd
Sira <siraphop_2538@hotmail.com>
2023-11-08 22:46:57 +1100

    Warn when some input is empty

--- script.js
+++ script.js
@@ -4,10 +4,38 @@ class formValidation {
   email = document.querySelector('.email');
   allInput = document.querySelectorAll('input');
   form = document.querySelector('form');
+  asianCountries = [
+    'Japan',
+    'China',
+    'India',
+    'South Korea',
+    'Thailand',
+    'Vietnam',
+    'Indonesia',
+    'Philippines',
+    'Singapore',
+    'Malaysia',
+    'Pakistan',
+    'Bangladesh',
+    'Sri Lanka',
+    'Nepal',
+    'Saudi Arabia',
+    'United Arab Emirates',
+    'Turkey',
+    'Iran',
+    'Iraq',
+    'Israel',
+    'Jordan',
+    'Lebanon',
+    'Oman',
+    'Qatar',
+    'Kuwait',
+    'Bahrain',
+  ];
 
   constructor() {
     this._emailValidation();
-    this._emptyEmail();
+    this._checkEmpty();
   }
 
   //Email
@@ -21,15 +49,17 @@ class formValidation {
         this.email.setCustomValidity('');
       }
 
-      this._emptyEmail();
+      this._checkEmpty();
     });
   }
 
-  _emptyEmail() {
-    if (this.email.validity.valueMissing) {
-      this.email.setCustomValidity('Please fill in Email');
-    } else {
-      console.log('Email is not empty');
+  _checkEmpty() {
+    for (const element of this.allInput) {
+      if (element.validity.valueMissing) {
+        element.setCustomValidity('Please fill in the blank');
+      } else {
+        element.setCustomValidity('');
+      }
     }
   }
 }
